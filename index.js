0f6057955adeb61262c1e2fbf8c5ca048479e265
Property404 <martinez.dagan@gmail.com>
2017-03-28 15:17:00 -0400

    Sign in functionality

--- index.js
+++ index.js
@@ -1,17 +1,119 @@
 const express = require("express");
+var session = require("express-session");
+var path = require("path");
+var hash = require("pbkdf2-password")();
+var bodyParser = require("body-parser");
 
 /* Set up app */
 var app = express();
 //~Set embedded JS view engine 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+// Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: 'shhh, very secret'
+}));
+
+// Session-persisted message middleware
+app.use(function(req, res, next){
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+  next();
+});
+
+// Dummy
+var users = {
+  tj: { name: 'tj' }
+};
+hash({ password: 'foobar' }, function (err, pass, salt, hash) {
+  if (err) throw err;
+  // store the salt & hash in the "db"
+  users.tj.salt = salt;
+  users.tj.hash = hash;
+});
+
+// Authenticate using our plain-object database of doom!
+function authenticate(name, pass, fn) {
+  if (!module.parent) console.log('authenticating %s:%s', name, pass);
+  var user = users[name];
+  // query the db for the given username
+  if (!user) return fn(new Error('cannot find user'));
+  // apply the same algorithm to the POSTed password, applying
+  // the hash against the pass / salt, if there is a match we
+  // found the user
+  hash({ password: pass, salt: user.salt }, function (err, pass, salt, hash) {
+    if (err) return fn(err);
+    if (hash == user.hash) return fn(null, user);
+    fn(new Error('invalid password'));
+  });
+}
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/sign-in');
+  }
+}
+app.get('/restricted', restrict, function(req, res){
+  res.send('Wahoo! restricted area, click to <a href="/logout">logout</a>');
+});
+app.get('/sign-out', function(req, res){
+  // destroy the user's session to log them out
+  // will be re-created next request
+  req.session.destroy(function(){
+    res.redirect('/sign-in');
+  });
+});
+
+app.post('/sign-in-post', function(req, res){
+console.log("Oy");
+  authenticate(req.body.username, req.body.password, function(err, user){
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation
+      req.session.regenerate(function(){
+        // Store the user's primary key
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        req.session.user = user;
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect('/dashboard');
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      //res.redirect('/sign-in');
+    }
+  });
+});
 // Static Pages
 app.use("/static", express.static("./static"));
 
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
 // Render views
 app.get("/*", function(req, res){
 	console.log("OK - View");
-	var url = req.url == "/" ? "/index" : req.url
+	var url = req.url == "/" ? "/home" : req.url
 	res.render(url.substr(1), {});
 });
 
