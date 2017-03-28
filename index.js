964da3f4fe19247a1b4513bb83d2c8bafec896be
Property404 <martinez.dagan@gmail.com>
2017-03-28 13:32:13 -0400

    Got basic views

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const expres = require("express");
+const express = require("express");
 
 /* Set up app */
 var app = express();
@@ -8,3 +8,13 @@ app.set("views", "./views");
 // Static Pages
 app.use("/static", express.static("./static"));
 
+// Render views
+app.get("/*", function(req, res){
+	console.log("OK - View");
+	var url = req.url == "/" ? "/index" : req.url
+	res.render(url.substr(1), {});
+});
+
+app.listen(80, function() {
+	console.log("Blog server running...");
+});
