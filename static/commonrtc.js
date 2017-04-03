86173df8ba7dd065be1c62f5cbfe5fd45f4734ed
Property404 <daganboy@gmail.com>
2017-04-03 16:17:09 +0000

    peerjs

--- static/commonrtc.js
+++ static/commonrtc.js
@@ -1,2 +1,2 @@
-peerjs_data = {host:"192.168.0.7", port: 9000,path: "/"}
+peerjs_data = {host:"162.243.109.160", port: 9000,path: "/"}
 var constraints = { audio: true, video: { width: 1280, height: 720 } }
