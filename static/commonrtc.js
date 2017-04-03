1baa0ac3db0722031cf98b82f238137249187d4d
Property404 <martinez.dagan@gmail.com>
2017-04-03 12:11:05 -0400

    Adding anything not added

---
+++ static/commonrtc.js
@@ -0,0 +1,2 @@
+peerjs_data = {host:"192.168.0.7", port: 9000,path: "/"}
+var constraints = { audio: true, video: { width: 1280, height: 720 } }
---
+++ static/test/commonrtc.js
@@ -0,0 +1,2 @@
+peerjs_data = {host:"localhost", port: 9000,path: "/"}
+var constraints = { audio: true, video: { width: 1280, height: 720 } }
